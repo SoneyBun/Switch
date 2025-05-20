d753a69736480aa29eb8d2778dde3280c7ef71a6
Sunny <addrosity.yt@gmail.com>
2025-05-20 10:21:52 -0400

    Create Question1.java

---
+++ Questions/Question1.java
@@ -0,0 +1,16 @@
+public class Question1.java {
+    public static void main(String[] args) {
+        int apples = 2;
+        double bananas = 4;
+        String totalFruit = apples + bananas;
+        int halfTotalFruit = totalFruit / 2;
+        System.out.print(halfTotalFruit); // What will be printed?
+    }
+}
+
+/** Answers
+ * A | 3
+ * B | apples + bananas / 2
+ * C | totalFruit / 2
+ * D | [Error]
+**/

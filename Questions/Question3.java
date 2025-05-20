48f26b5a603284d63025beb776bd0b354b0d72ae
Sunny <addrosity.yt@gmail.com>
2025-05-20 10:33:11 -0400

    Create Question3.java

---
+++ Questions/Question3.java
@@ -0,0 +1,17 @@
+public class Question3 {
+    public static void main(String[] args) {
+        double a = 5.7;
+        double b = 4.4 / 2;
+        double c = 3.2;
+        double d = a + b + c;
+        int e = (int) d;
+        System.out.println(e); // What will be printed?
+    }
+}
+
+/** Answers
+ * A | 12
+ * B | 11
+ * C | 11.00
+ * D | 12.00
+**/

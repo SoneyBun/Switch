9dcf7b3b23af1ab4d4166434e8956f2b3709a13e
Sunny <addrosity.yt@gmail.com>
2025-05-20 10:26:16 -0400

    Create Question2.java

---
+++ Questions/Question2.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+
+public class Question2 {
+    public static void main(String[] args) {
+        int rand = (int) (Math.random() * 10); // What is the range of rand?
+    }
+}
+
+/** Answers
+ * A | 1-11
+ * B | 0-10
+ * C | 0-9
+ * D | 1-10
+**/

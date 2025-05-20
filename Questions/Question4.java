d30b6f1cec223381162a98318f88d03aed105388
Sunny <addrosity.yt@gmail.com>
2025-05-20 13:29:22 -0400

    Create Question4.java

---
+++ Questions/Question4.java
@@ -0,0 +1,16 @@
+public class Question4 {
+    private int a;
+    private int b;
+    
+    public Question4(int first, int second) { // What is this method?
+        a = first;
+        b = second;
+    }
+}
+
+/** Answers
+ * A | Default Constructor
+ * B | Setter Method
+ * C | Getter Method
+ * D | Parameter Constructor
+**/
